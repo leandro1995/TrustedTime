4b2d199aadc055bfcbcc67ab39d8d3833ca2107e
Leandro Castillo Borja <leccbo1995@gmail.com>
2025-02-20 22:19:23 -0500

    Ordenar dimenciones

---
+++ app/src/main/java/com/leandro1995/trustedtime/ui/theme/Dimen.kt
@@ -0,0 +1,7 @@
+package com.leandro1995.trustedtime.ui.theme
+
+import androidx.compose.ui.unit.sp
+
+val SP_0_5 = 0.5.sp
+val SP_16 = 16.sp
+val SP_24 = 24.sp
--- app/src/main/java/com/leandro1995/trustedtime/ui/theme/Type.kt
+++ app/src/main/java/com/leandro1995/trustedtime/ui/theme/Type.kt
@@ -4,14 +4,13 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = SP_16,
+        lineHeight = SP_24,
+        letterSpacing = SP_0_5
     )
 )
